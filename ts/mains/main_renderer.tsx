bc689153fc7e621320c0f5bb7225443c0275fe3b
Ian Macdonald <ian@caliban.org>
2022-03-01 16:46:23 +0100

    Add settings toggle for per message timestamps.

--- ts/mains/main_renderer.tsx
+++ ts/mains/main_renderer.tsx
@@ -148,6 +148,11 @@ Storage.onready(async () => {
       await Storage.put('frequent-timestamps', value);
     },
 
+    getPerMessageTimestamps: () => Storage.get('per-message-timestamps', true),
+    setPerMessageTimestamps: async (value: boolean) => {
+      await Storage.put('per-message-timestamps', value);
+    },
+
     getSpellCheck: () => Storage.get('spell-check', true),
     setSpellCheck: async (value: boolean) => {
       await Storage.put('spell-check', value);
@@ -323,6 +328,12 @@ async function start() {
     const currentValue = window.getSettingValue('frequent-timestamps');
     const newValue = currentValue !== undefined ? !currentValue : true;
     window.Events.setFrequentTimestamps(newValue);
+  };
+
+  window.togglePerMessageTimestamps= () => {
+    const currentValue = window.getSettingValue('per-message-timestamps');
+    const newValue = currentValue !== undefined ? !currentValue : true;
+    window.Events.setPerMessageTimestamps(newValue);
   };
 
   window.toggleSpellCheck = () => {
